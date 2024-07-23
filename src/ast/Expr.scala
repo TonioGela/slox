43da3ddd950b8459b076b195c6d3ecbce55427df
Antonio Gelameris <toniogela89@gmail.com>
2024-07-23 15:11:48 +0200

    Add all the elements for the future AST

--- src/ast/Expr.scala
+++ src/ast/Expr.scala
@@ -3,16 +3,33 @@ package dev.toniogela.lox.ast
 import dev.toniogela.lox.scanner.*
 
 trait Visitor[T]:
+  // def visitAssignExpr(expr: Assign): T
   def visitBinaryExpr(expr: Binary): T
+  // def visitCallExpr(expr: Call): T
+  // def visitGetExpr(expr: Get): T
   def visitGroupingExpr(expr: Grouping): T
   def visitLiteralExpr(expr: Literal): T
+  // def visitLogicalExpr(expr: Logical): T
+  // def visitSetExpr(expr: Set): T
+  // def visitSuperExpr(expr: Super): T
+  // def visitThisExpr(expr: This): T
   def visitUnaryExpr(expr: Unary): T
+  // def visitVariableExpr(expr: Variable): T
 
 sealed trait Expr:
   def accept[T](visitor: Visitor[T]): T
 
+// case class Assign(name: Token, value: Expr) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitAssignExpr(this)
+
 case class Binary(left: Expr, operator: Token, right: Expr) extends Expr:
   override def accept[T](visitor: Visitor[T]): T = visitor.visitBinaryExpr(this)
+
+// case class Call(callee: Expr, paren: Token, arguments: List[Expr]) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitCallExpr(this)
+
+// case class Get(item: Expr, name: Token) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitGetExpr(this)
 
 case class Grouping(expression: Expr) extends Expr:
   override def accept[T](visitor: Visitor[T]): T = visitor.visitGroupingExpr(this)
@@ -20,5 +37,20 @@ case class Grouping(expression: Expr) extends Expr:
 case class Literal(value: Any) extends Expr:
   override def accept[T](visitor: Visitor[T]): T = visitor.visitLiteralExpr(this)
 
+// case class Logical(left: Expr, operator: Token, right: Expr) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitLogicalExpr(this)
+
+// case class Set(item: Expr, name: Token, value: Expr) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitSetExpr(this)
+
+// case class Super(keyword: Token, method: Token) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitSuperExpr(this)
+
+// case class This(keyword: Token) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitThisExpr(this)
+
 case class Unary(operator: Token, right: Expr) extends Expr:
   override def accept[T](visitor: Visitor[T]): T = visitor.visitUnaryExpr(this)
+
+// case class Variable(name: Token) extends Expr:
+//   override def accept[T](visitor: Visitor[T]): T = visitor.visitVariableExpr(this)
--- src/ast/Stmt.scala
+++ src/ast/Stmt.scala
@@ -1,21 +1,44 @@
 package dev.toniogela.lox.ast
 
+import dev.toniogela.lox.scanner.Token
+
 trait StmtVisitor[T]:
-//   def visitBlockStmt(stmt: Block): T
-//   def visitClassStmt(stmt: Clazz): T
+  // def visitBlockStmt(stmt: Block): T
+  // def visitClassStmt(stmt: Clazz): T
   def visitExpressionStmt(stmt: Expression): T
-//   def visitFunctionStmt(stmt: Function): T
-//   def visitIfStmt(stmt: If): T
+  // def visitFunctionStmt(stmt: Function): T
+  // def visitIfStmt(stmt: If): T
   def visitPrintStmt(stmt: Print): T
-//   def visitReturnStmt(stmt: Return): T
-//   def visitVarStmt(stmt: Var): T
-//   def visitWhileStmt(stmt: While): T
+  // def visitReturnStmt(stmt: Return): T
+  // def visitVarStmt(stmt: Var): T
+  // def visitWhileStmt(stmt: While): T
 
 sealed trait Stmt:
   def accept[T](visitor: StmtVisitor[T]): T
 
+// case class Block(statements: List[Stmt]) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitBlockStmt(this)
+
+// case class Clazz(name: Token, superclass: Variable, methods: List[Function]) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitClassStmt(this)
+
 case class Expression(expr: Expr) extends Stmt:
   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitExpressionStmt(this)
 
+// case class Function(name: Token, params: List[Token], body: List[Stmt]) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitFunctionStmt(this)
+
+// case class If(condition: Expr, thenBranch: Stmt, elseBranch: Stmt) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitIfStmt(this)
+
 case class Print(expr: Expr) extends Stmt:
   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitPrintStmt(this)
+
+// case class Return(keyword: Token, value: Expr) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitReturnStmt(this)
+
+// case class Var(name: Token, initializer: Expr) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitVarStmt(this)
+
+// case class While(condition: Expr, body: List[Stmt]) extends Stmt:
+//   override def accept[T](visitor: StmtVisitor[T]): T = visitor.visitWhileStmt(this)
